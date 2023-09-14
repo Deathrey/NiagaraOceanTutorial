912a0fdf1926d28af2d45c83b3771046697c58a6
unreal79 <unreal79@gmail.com>
2023-09-14 22:46:56 +0500

    add relative path

---
+++ Source/ShaderMacro/ShaderMacroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShaderMacroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADERMACRO_API AShaderMacroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
